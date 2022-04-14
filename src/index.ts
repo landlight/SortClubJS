9fb7fb31059db69f6956fbec615a9d0b56f8952b
landlight <ck.light@gmail.com>
2022-04-14 14:13:59 +0700

    :sparkles: add time logs

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,22 @@ import { SortingTypes, SortOrder } from "./types";
 console.log("hello, i will write the sorting methods and description here \n");
 
 const unsortedArray = [9, 8, 4, 3, 6, 5, 2, 1, 54, 43, 19, 17];
-
+console.time(SortingTypes.BUBBLE_SORT);
 console.log(SortingTypes.BUBBLE_SORT, bubbleSort(unsortedArray, SortOrder.ASC), SortOrder.ASC);
 console.log(SortingTypes.BUBBLE_SORT, bubbleSort(unsortedArray, SortOrder.DSC), SortOrder.DSC);
+console.timeEnd(SortingTypes.BUBBLE_SORT);
 
+console.time(SortingTypes.SELECTION_SORT);
 console.log(SortingTypes.SELECTION_SORT, selectionSort(unsortedArray, SortOrder.ASC), SortOrder.ASC);
 console.log(SortingTypes.SELECTION_SORT, selectionSort(unsortedArray, SortOrder.DSC), SortOrder.DSC);
+console.timeEnd(SortingTypes.SELECTION_SORT);
 
+console.time(SortingTypes.INSERTION_SORT);
 console.log(SortingTypes.INSERTION_SORT, insertionSort(unsortedArray, SortOrder.ASC), SortOrder.ASC);
 console.log(SortingTypes.INSERTION_SORT, insertionSort(unsortedArray, SortOrder.DSC), SortOrder.DSC);
+console.timeEnd(SortingTypes.INSERTION_SORT);
 
+console.time(SortingTypes.MERGE_SORT);
 console.log(SortingTypes.MERGE_SORT, mergeSort(unsortedArray, SortOrder.ASC), SortOrder.ASC);
 console.log(SortingTypes.MERGE_SORT, mergeSort(unsortedArray, SortOrder.DSC), SortOrder.DSC);
+console.timeEnd(SortingTypes.MERGE_SORT);
